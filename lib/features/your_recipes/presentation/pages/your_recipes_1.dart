8105e1d59d6093968e5e30fe72a4c28f6b213b3d
ozodbekabdullajonov12 <ozodbekabdullajonov1@gmail.com>
2025-03-26 17:31:09 +0500

    banzaay

---
+++ lib/features/your_recipes/presentation/pages/your_recipes_1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class YOurecipe extends StatelessWidget {
+  const YOurecipe({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
